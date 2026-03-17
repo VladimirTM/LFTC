529fa39788e8e6ce867c46f6ecc8bb24ecb16768
VladimirTM <vladimirghimpau@gmail.com>
2026-03-17 12:14:21 +0200

    Am adaugat cuvintele cheie, delimitatorii si comentariile

--- AtomC/lexer.c
+++ AtomC/lexer.c
@@ -124,6 +124,40 @@ Token *tokenize(const char *pch)
 				pch++;
 			}
 			break;
+		case '&':
+			if (pch[1] == '&')
+			{
+				addTk(AND);
+				pch += 2;
+			}
+			else
+			{
+				err("invalid char: %c (%d)", *pch, *pch);
+			}
+			break;
+		case '|':
+			if (pch[1] == '|')
+			{
+				addTk(OR);
+				pch += 2;
+			}
+			else
+			{
+				err("invalid char: %c (%d)", *pch, *pch);
+			}
+			break;
+		case '!':
+			if (pch[1] == '=')
+			{
+				addTk(NOTEQ);
+				pch += 2;
+			}
+			else
+			{
+				addTk(NOT);
+				pch++;
+			}
+			break;
 		case '=':
 			if (pch[1] == '=')
 			{
@@ -136,6 +170,30 @@ Token *tokenize(const char *pch)
 				pch++;
 			}
 			break;
+		case '<':
+			if (pch[1] == '=')
+			{
+				addTk(LESSEQ);
+				pch += 2;
+			}
+			else
+			{
+				addTk(LESS);
+				pch++;
+			}
+			break;
+		case '>':
+			if (pch[1] == '=')
+			{
+				addTk(GREATEREQ);
+				pch += 2;
+			}
+			else
+			{
+				addTk(GREATER);
+				pch++;
+			}
+			break;
 		default:
 			if (isalpha(*pch) || *pch == '_')
 			{
@@ -144,12 +202,54 @@ Token *tokenize(const char *pch)
 				}
 				char *text = extract(start, pch);
 				if (strcmp(text, "char") == 0)
+				{
+					free(text);
 					addTk(TYPE_CHAR);
+				}
+				else if (strcmp(text, "double") == 0)
+				{
+					free(text);
+					addTk(TYPE_DOUBLE);
+				}
+				else if (strcmp(text, "else") == 0)
+				{
+					free(text);
+					addTk(ELSE);
+				}
+				else if (strcmp(text, "if") == 0)
+				{
+					free(text);
+					addTk(IF);
+				}
+				else if (strcmp(text, "int") == 0)
+				{
+					free(text);
+					addTk(TYPE_INT);
+				}
+				else if (strcmp(text, "return") == 0)
+				{
+					free(text);
+					addTk(RETURN);
+				}
+				else if (strcmp(text, "struct") == 0)
+				{
+					free(text);
+					addTk(STRUCT);
+				}
+				else if (strcmp(text, "void") == 0)
+				{
+					free(text);
+					addTk(VOID);
+				}
+				else if (strcmp(text, "while") == 0)
+				{
+					free(text);
+					addTk(WHILE);
+				}
 				else
 				{
 					tk = addTk(ID);
 					tk->text = text;
-					free(text);
 				}
 			}
 			else
@@ -160,8 +260,44 @@ Token *tokenize(const char *pch)
 
 void showTokens(const Token *tokens)
 {
+	static const char *names[] = {
+		"ID",
+		"TYPE_CHAR",
+		"TYPE_DOUBLE",
+		"ELSE",
+		"IF",
+		"TYPE_INT",
+		"RETURN",
+		"STRUCT",
+		"VOID",
+		"WHILE",
+		"INT",
+		"DOUBLE",
+		"CHAR",
+		"STRING",
+		"COMMA",
+		"SEMICOLON",
+		"LPAR",
+		"RPAR",
+		"LBRACKET",
+		"RBRACKET",
+		"LACC",
+		"RACC",
+		"END",
+		"ADD",
+		"SUB",
+		"MUL",
+		"DIV",
+		"DOT",
+		"AND",
+		"OR",
+		"NOT",
+		"ASSIGN",
+		"EQUAL",
+		"NOTEQ",
+		"LESS", "LESSEQ", "GREATER", "GREATEREQ"};
 	for (const Token *tk = tokens; tk; tk = tk->next)
 	{
-		printf("%d\n", tk->code);
+		printf("%d\t%s\n", tk->code, names[tk->code]);
 	}
 }
